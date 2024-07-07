825ded88ec824fda7893e4357398a6cb6741f86d
musaubenjamin1 <161810005+musaubenjamin1@users.noreply.github.com>
2024-07-07 10:41:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="benjaminkaptire@gmail.com"
-global.location="Lahore,Pakistan."
+global.location="Kampala/Uganda."
 
 
 global.mongodb= process.env.MONGODB_URI || ""
